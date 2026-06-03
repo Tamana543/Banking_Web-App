5cfcb679715bf7ba709a473f2a8f341fdedc198f
Tamana543 <tamanafarzami33@gmail.com>
2026-06-03 14:50:35 +0600

    CTA updated

--- frontend/src/components/home/CTA.jsx
+++ frontend/src/components/home/CTA.jsx
@@ -1,7 +1,16 @@
+import { useNavigate } from "react-router-dom";
+
+
 function CTA() {
+     const navigate = useNavigate();
+
   return (
     <section>
-      <h2>Open Your Free Account Today</h2>
+       <h2>Open Your Free Account Today</h2>
+
+      <button onClick={() => navigate("/register")}>
+        Open Account
+      </button>
     </section>
   );
 }
