d13c77114bf386cb38d7a0ddf6cb5aac93090cca
Tamana543 <tamanafarzami33@gmail.com>
2026-06-04 14:15:27 +0600

    navar jsx updated

--- frontend/src/components/Landing/Navbar.jsx
+++ frontend/src/components/Landing/Navbar.jsx
@@ -1,27 +1,37 @@
-import {Link } from "react-router-dom"
+import { Link } from "react-router-dom";
 
-function Navbar(){
-return (
-     <nav class="nav">
-        {/* <img
-          src="img/logo.png"
-          alt="Bankist logo"
-          class="nav__logo"
-          id="logo"
-        /> */}
-        <ul class="nav__link s">
-          <li class="nav__item">
-            <Link class="nav__link " to="/">Home</Link >
-          </li>
-          <li class="nav__item">
-            <Link class="nav__link " to="/login">Login</Link >
-          </li>
-          <li class="nav__item">
-            <Link class="nav__link  nav__link --btn btn--show-modal" to="#"
-              >Open account</Link >
-          </li>
-        </ul>
-      </nav>
-);
+function Navbar() {
+  return (
+    <nav className="nav">
+      <div className="nav__logo">
+        <h2>Bankist Pro</h2>
+      </div>
+
+      <ul className="nav__links">
+        <li>
+          <a href="#features">Features</a>
+        </li>
+
+        <li>
+          <a href="#operations">Operations</a>
+        </li>
+
+        <li>
+          <a href="#testimonials">Testimonials</a>
+        </li>
+
+        <li>
+          <Link to="/login">Login</Link>
+        </li>
+
+        <li>
+          <Link to="/register" className="nav__btn">
+            Open Account
+          </Link>
+        </li>
+      </ul>
+    </nav>
+  );
 }
-export default Navbar
+
+export default Navbar;
