fda485796937d8697dbae150670fb3b0d4ca4720
Tamana543 <tamanafarzami33@gmail.com>
2026-06-06 19:52:45 +0600

    CTA Updated

--- frontend/src/components/Landing/CTA.jsx
+++ frontend/src/components/Landing/CTA.jsx
@@ -1,17 +1,23 @@
 import { useNavigate } from "react-router-dom";
 
 
+
 function CTA() {
      const navigate = useNavigate();
 
   return (
-    <section>
-       <h2>Open Your Free Account Today</h2>
-
+     <section class="section section--sign-up">
+      <div class="section__title">
+        <h3 class="section__header">
+          The best day to join Bankist was one year ago. The second best is
+          today!
+        </h3>
+      </div>
       <button onClick={() => navigate("/register")}>
         Open Account
       </button>
     </section>
+    
   );
 }
 
