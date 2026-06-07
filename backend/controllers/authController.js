f5fb77edffd95351a6814c9fce24cf08eeb4d72c
Tamana543 <tamanafarzami33@gmail.com>
2026-06-07 14:45:26 +0600

    auth controller updated: registerUser created

---
+++ backend/controllers/authController.js
@@ -0,0 +1,47 @@
+import bcrypt from "bcryptjs";
+import User from "../models/User";
+
+export const registerUser = async (req,res)=>{
+     try {
+          const {firstName,lastName,email,password,pin} = req.body;
+
+          //User existance checker
+          const existingUser = await User.findOne({email});
+
+          if (existingUser){
+               return res.status(400).json({
+                    success : false,
+                    message : "User already exists"
+               })
+          }
+
+          // passwrd hash generator
+          const hashedPassword = await bcrypt.hash(password,10) ;
+
+          // User Creation 
+          const user = await User.create({
+               firstName, 
+               lastName,
+               email, 
+               password: hashedPassword,
+               pin
+          })
+          res.status(201).json({
+               success : true,
+               message : "Account created successfully.",
+               user : {
+                    id: user._id,
+                    firstName: user.firstName,
+                    lastName: user.lastName,
+                    email: user.email,
+               },
+
+          });
+     } catch (error) {
+          console.error(`error from registerUser ${error}`)
+          res.status(500).json({
+          success: false,
+          message: "Server Error",
+    });
+     }
+}
