b9431b033e3cc57e71ff813d7ce24b7dfb48d6db
Tamana543 <tamanafarzami33@gmail.com>
2026-06-07 14:51:11 +0600

    auth Route updated: forst API auth/register created

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,8 @@ import dotenv from "dotenv"
 import connectDB from "./config/db.js"
 import dns from 'node:dns'; // for error "Error from db.js : querySrv ECONNREFUSED _mongodb._tcp.cluster0.ufecoqb.mongodb.net" "
 dns.setServers(['1.1.1.1', '8.8.8.8']);
+import authRouter from "./routes/authRoutes.js"
+
 
 
 dotenv.config()
@@ -10,6 +12,8 @@ dotenv.config()
 connectDB()
 
 const app = express();
+app.unsubscribe(express.json())
+app.use("/api/auth",authRouter)
 
 app.get("/", (req, res) => {
   res.send("Bankist API Running...");
