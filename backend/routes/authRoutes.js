2c54121d537bcb9ff2dcf33a119280c8fbbfaa55
Tamana543 <tamanafarzami33@gmail.com>
2026-06-07 15:02:14 +0600

    authroutes updated

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,7 +1,7 @@
 import express from "express";
 import { registerUser } from "../controllers/authController";
-
+import { registerUser, loginUser, } from "../controllers/authController.js";
 const router = express.Router();
 router.post("/register", registerUser)
-
+router.post("/login", loginUser);
 export default router
