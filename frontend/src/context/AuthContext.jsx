3ea3f5151c1f1397d8c7ed57b8847a35e9584200
Tamana543 <tamanafarzami33@gmail.com>
2026-06-13 06:59:54 +0600

    The logout timer updated

--- frontend/src/context/AuthContext.jsx
+++ frontend/src/context/AuthContext.jsx
@@ -1,10 +1,11 @@
-import { createContext, useContext, useEffect, useState } from "react";
+import { createContext, useContext, useEffect, useState, useRef } from "react";
 
 const AuthContext = createContext();
 
 export function AuthProvider({ children }) {
   const [user, setUser] = useState(null);
   const [token, setToken] = useState(null);
+  const logoutTimer = useRef(null); // Timer for the automatic logout 
 
   useEffect(() => {
     const storedUser = localStorage.getItem("user");
@@ -13,18 +14,31 @@ export function AuthProvider({ children }) {
     if (storedUser && storedToken) {
       setUser(JSON.parse(storedUser));
       setToken(storedToken);
+      startLogoutTimer()
     }
   }, []);
-
+  const startLogoutTimer = ()=>{
+    if(logoutTimer.current){
+      clearTimeout(logoutTimer.current)
+    }
+    // Logout after x days (for me I got 6)
+    logoutTimer.current = setTimeout(()=>{
+      logout();
+    },6 * 24 * 60 * 60 * 1000)
+  }
   const login = (userData, jwtToken) => {
     localStorage.setItem("user", JSON.stringify(userData));
     localStorage.setItem("token", jwtToken);
 
     setUser(userData);
     setToken(jwtToken);
+    startLogoutTimer();
   };
 
   const logout = () => {
+     if (logoutTimer.current) {
+    clearTimeout(logoutTimer.current);
+  }
     localStorage.removeItem("user");
     localStorage.removeItem("token");
 
