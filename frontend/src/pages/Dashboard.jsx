86c87a26423c3786bea36248ac252f21e7384be4
Tamana543 <tamanafarzami33@gmail.com>
2026-06-13 06:52:51 +0600

    logout UI (basic) and the main logout function added: remove saved user, remoce stored JWT, clear the react status

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,5 +1,24 @@
+import { useNavigate } from "react-router-dom";
+import { useAuth } from "../context/AuthContext";
+
 function Dashboard() {
-  return <h1>Dashboard Page</h1>;
+  const navigate = useNavigate();
+  const { user, logout } = useAuth();
+
+  const handleLogout = () => {
+    logout();
+    navigate("/login");
+  };
+
+  return (
+    <div>
+      <h1>Welcome, {user?.firstName}!</h1>
+
+      <button onClick={handleLogout}>
+        Logout
+      </button>
+    </div>
+  );
 }
 
 export default Dashboard;
