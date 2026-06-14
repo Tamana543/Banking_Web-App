4ebcfd0f717b568ade600ec36a83b785ce302bee
Tamana543 <tamanafarzami33@gmail.com>
2026-06-14 15:22:10 +0600

    folder updated

--- backend/server.js
+++ backend/server.js
@@ -13,6 +13,13 @@ connectDB();
 
 const app = express();
 
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
