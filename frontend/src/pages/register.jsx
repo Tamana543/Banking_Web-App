8920ea8391bb3a7daf191ffbbf68c4a9a92983b3
Tamana543 <tamanafarzami33@gmail.com>
2026-06-15 16:59:28 +0600

    register UI updated

--- frontend/src/pages/register.jsx
+++ frontend/src/pages/register.jsx
@@ -1,5 +1,101 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+import { registerUser } from "../api/authApi";
+import { useAuth } from "../context/AuthContext";
+
 function Register() {
-  return <h1>Register Page</h1>;
+  const navigate = useNavigate();
+  const { login } = useAuth();
+
+    const [formData, setFormData] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+    pin: "",
+  });
+
+  const handleChange = (e) => {
+    setFormData((prev) => ({
+      ...prev,
+      [e.target.name]: e.target.value,
+    }));
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const data = await registerUser(formData);
+
+      // Automatically log the user in after registration
+      login(data.user, data.token);
+
+      navigate("/dashboard");
+    } catch (error) {
+      alert(error.message);
+    }
+  };
+
+  return (
+    <div>
+      <h1>Create Account</h1>
+
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          name="firstName"
+          placeholder="First Name"
+          value={formData.firstName}
+          onChange={handleChange}
+        />
+
+        <input
+          type="text"
+          name="lastName"
+          placeholder="Last Name"
+          value={formData.lastName}
+          onChange={handleChange}
+        />
+
+        <input
+          type="email"
+          name="email"
+          placeholder="Email"
+          value={formData.email}
+          onChange={handleChange}
+        />
+
+        <input
+          type="password"
+          name="password"
+          placeholder="Password"
+          value={formData.password}
+          onChange={handleChange}
+        />
+
+        <input
+          type="password"
+          name="pin"
+          placeholder="4-digit PIN"
+          value={formData.pin}
+          onChange={handleChange}
+        />
+        <input
+          type="password"
+          name="confirmPassword"
+          placeholder="Confirm Password"
+          value={formData.confirmPassword}
+          onChange={handleChange}
+        />
+        <button type="submit">
+          Register
+        </button>
+      </form>
+    </div>
+  );
 }
 
 export default Register;
