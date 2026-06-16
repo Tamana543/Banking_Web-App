517d1e69e2d07c8123420a24b1d750fd57f87106
Tamana543 <tamanafarzami33@gmail.com>
2026-06-16 14:48:54 +0600

    Route udpated

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -128,6 +128,7 @@ export const loginUser = async (req, res) => {
   }
 };
 
+// Logic : the request okay (valid from JWT side) ? success : 401 Unauthorized
 export const getCurrentUser = async (req, res) => {
   try {
     res.status(200).json({
--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,9 +1,14 @@
 
 import express from "express";
 import validateRegister from "../middleware/validateRegister.js";
+import protect from "../middleware/authMiddleware.js";
 
-import { registerUser, loginUser, } from "../controllers/authController.js";
+
+import { registerUser, loginUser, getCurrentUser } from "../controllers/authController.js";
 const router = express.Router();
+
+
 router.post("/register", validateRegister, registerUser)
 router.post("/login", loginUser);
+router.get("/me",protect,getCurrentUser)
 export default router
