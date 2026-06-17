827132b3de49acc42f92da1e504c40ad54df7b6d
Tamana543 <tamanafarzami33@gmail.com>
2026-06-17 16:15:17 +0600

    balanced cards and stats cards jsx updated

---
+++ frontend/src/components/Dashnoard/BalancedCards.jsx
@@ -0,0 +1,13 @@
+function BalanceCard() {
+  return (
+    <section className="balance-card">
+      <h3>Current Balance</h3>
+
+      <h1>$0.00</h1>
+
+      <p>Available Balance</p>
+    </section>
+  );
+}
+
+export default BalanceCard;
---
+++ frontend/src/components/Dashnoard/StatsCards.jsx
@@ -0,0 +1,24 @@
+function StatsCards() {
+  return (
+    <section className="stats-cards">
+
+      <div className="stat-card">
+        <h4>Income</h4>
+        <h2>$0.00</h2>
+      </div>
+
+      <div className="stat-card">
+        <h4>Expenses</h4>
+        <h2>$0.00</h2>
+      </div>
+
+      <div className="stat-card">
+        <h4>Transactions</h4>
+        <h2>0</h2>
+      </div>
+
+    </section>
+  );
+}
+
+export default StatsCards;
