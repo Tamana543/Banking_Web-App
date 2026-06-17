8853494bff5c0e9f5ede851ab5730d12b48123e4
Tamana543 <tamanafarzami33@gmail.com>
2026-06-17 16:10:15 +0600

    redirecting logout to login page added

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -3,16 +3,20 @@ import { useAuth } from "../context/AuthContext";
 
 function Dashboard() {
   const navigate = useNavigate();
+
   const { user, logout } = useAuth();
 
   const handleLogout = () => {
     logout();
+
     navigate("/login");
   };
 
   return (
     <div>
-      <h1>Welcome, {user?.firstName}!</h1>
+      <h1>
+        Welcome {user?.firstName}
+      </h1>
 
       <button onClick={handleLogout}>
         Logout
