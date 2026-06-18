f326e53658fb3265df25451b0f5e93938108a415
Tamana543 <tamanafarzami33@gmail.com>
2026-06-18 22:49:18 +0600

    style updated

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-
+import "./styles/global.css";
 import App from "./App.jsx";
 
 import { AuthProvider } from "./context/AuthContext.jsx";
