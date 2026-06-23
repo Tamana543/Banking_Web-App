d9833ac6e18d324f68772b321f907a585599ed76
Tamana543 <tamanafarzami33@gmail.com>
2026-06-23 15:31:05 +0600

    transfer router added

--- backend/routes/transactionRoutes.js
+++ backend/routes/transactionRoutes.js
@@ -1,10 +1,11 @@
 import express from "express";
 import protect from "../middleware/authMiddleware.js";
-import { depositMoney,getTransactions } from "../controllers/transactionController.js";
+import { depositMoney,getTransactions,transferMoney } from "../controllers/transactionController.js";
 
 const router = express.Router();
 
 router.post("/deposit", protect, depositMoney );
+router.post("/transfer",protect,transferMoney)
 router.get("/",protect,getTransactions)
 
 export default router;
