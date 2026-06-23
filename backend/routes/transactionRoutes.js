a566a53a0750bc3a239d43fd7080ee04e5f45152
Tamana543 <tamanafarzami33@gmail.com>
2026-06-23 15:37:17 +0600

    withdraw router added

--- backend/routes/transactionRoutes.js
+++ backend/routes/transactionRoutes.js
@@ -1,11 +1,12 @@
 import express from "express";
 import protect from "../middleware/authMiddleware.js";
-import { depositMoney,getTransactions,transferMoney } from "../controllers/transactionController.js";
+import { depositMoney,getTransactions,transferMoney,withdrawMondy } from "../controllers/transactionController.js";
 
 const router = express.Router();
 
 router.post("/deposit", protect, depositMoney );
 router.post("/transfer",protect,transferMoney)
+router.post("/withdraw",protect,withdrawMondy)
 router.get("/",protect,getTransactions)
 
 export default router;
