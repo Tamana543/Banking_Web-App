d43808e23ee803a936688c21811d0d084cdaec43
Tamana543 <tamanafarzami33@gmail.com>
2026-06-28 14:02:28 +0600

    alert common component created :))))

---
+++ frontend/src/components/common/AlertMessage.jsx
@@ -0,0 +1,11 @@
+function AlertMessage({ type = "error", message }) {
+  if (!message) return null;
+
+  return (
+    <div className={`alert-message ${type}`}>
+      {message}
+    </div>
+  );
+}
+
+export default AlertMessage;
