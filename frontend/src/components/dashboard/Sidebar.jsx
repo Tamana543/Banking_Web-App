ff6259e5454125fcf73234cfefc2212cd61dadb4
Tamana543 <tamanafarzami33@gmail.com>
2026-07-09 16:59:05 +0600

    side bar component updated

--- frontend/src/components/dashboard/Sidebar.jsx
+++ frontend/src/components/dashboard/Sidebar.jsx
@@ -9,52 +9,69 @@ import {
   Settings,
 } from "lucide-react";
 
-function Sidebar() {
+function Sidebar({
+  sidebarOpen,
+  setSidebarOpen
+}) {
   return (
-    <aside className="sidebar">
+    <aside className={`sidebar ${sidebarOpen? "open": ""}`}>
       <div className="sidebar-logo">
         <h2>BANKIST</h2>
         <span>PRO</span>
       </div>
 
       <nav className="sidebar-nav">
-        <NavLink to="/dashboard">
+        <NavLink to="/dashboard" onClick={() => setSidebarOpen(false) }>
           <LayoutDashboard size={20} />
           <span>Dashboard</span>
         </NavLink>
 
-        <NavLink to="/transactions">
+        <NavLink to="/transactions" onClick={() => setSidebarOpen(false) }>
           <ArrowRightLeft size={20} />
           <span>Transactions</span>
         </NavLink>
 
-        <NavLink to="/transfer">
+        <NavLink to="/transfer" onClick={() => setSidebarOpen(false) }>
           <ArrowRightLeft size={20} />
           <span>Transfer</span>
         </NavLink>
 
-        <NavLink to="/loans">
+        <NavLink to="/loans" onClick={() => setSidebarOpen(false) }>
           <Landmark size={20} />
           <span>Loans</span>
         </NavLink>
 
-        <NavLink to="/analytics">
+        <NavLink to="/analytics" onClick={() => setSidebarOpen(false) }>
           <ChartColumn size={20} />
           <span>Analytics</span>
         </NavLink>
 
-        <NavLink to="/profile">
+        <NavLink to="/profile" onClick={() => setSidebarOpen(false) }>
           <UserRound size={20} />
           <span>Profile</span>
         </NavLink>
 
-        <NavLink to="/settings">
+        <NavLink to="/settings" onClick={() => setSidebarOpen(false) }>
           <Settings size={20} />
           <span>Settings</span>
         </NavLink>
       </nav>
     </aside>
-  );
+    
+
+sidebarOpen && (
+
+<div
+
+className="sidebar-overlay"
+
+onClick={() =>
+    setSidebarOpen(false)
+}
+
+/>
+
+));
 }
 
 export default Sidebar;
