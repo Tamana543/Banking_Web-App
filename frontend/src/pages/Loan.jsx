c608511a3eb2ee5fc382d4b053fa4e84bf89b97e
Tamana543 <tamanafarzami33@gmail.com>
2026-07-11 10:56:51 +0600

    Loan.jsx updated

--- frontend/src/pages/Loan.jsx
+++ frontend/src/pages/Loan.jsx
@@ -1,1 +1,33 @@
 import "../styles/loan.css"
+import { useState } from "react";
+import Sidebar from "../components/dashboard/Sidebar";
+import DashboardHeader from "../components/dashboard/DashboardHeader";
+import HamburgerButton from "../components/dashboard/HamburgerButton";
+import "../styles/dashboard/dashboard.css";
+function loan(){
+    const [sidebarOpen, setSidebarOpen] = useState(false);
+    const [amount, setAmount] = useState("");
+    const [purpose, setPurpose] = useState("");
+    const [duration, setDuration] = useState("");
+    return (
+          <div className="dashboard-layout">
+          <HamburgerButton
+               sidebarOpen={sidebarOpen}
+               onClick={() =>
+                    setSidebarOpen(!sidebarOpen)
+               }
+          />
+          <Sidebar
+               sidebarOpen={sidebarOpen}
+               setSidebarOpen={setSidebarOpen}
+          />
+          <main className="dashboard">
+               <DashboardHeader />
+               <section className="loan-page">
+                    <div className="loan-card">
+                    </div>
+               </section>
+          </main>
+          </div>
+)
+}
