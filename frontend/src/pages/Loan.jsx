413842587e4014e5337292b2daa1e1785bc6bd54
Tamana543 <tamanafarzami33@gmail.com>
2026-07-11 11:15:57 +0600

    Loan.jsx updated

--- frontend/src/pages/Loan.jsx
+++ frontend/src/pages/Loan.jsx
@@ -1,33 +1,33 @@
-import "../styles/loan.css"
-import { useState } from "react";
-import Sidebar from "../components/dashboard/Sidebar";
+import { useEffect, useState } from "react";
+
+import DashboardLayout from "../components/dashboard/DashboardLayout";
 import DashboardHeader from "../components/dashboard/DashboardHeader";
-import HamburgerButton from "../components/dashboard/HamburgerButton";
-import "../styles/dashboard/dashboard.css";
-function loan(){
-    const [sidebarOpen, setSidebarOpen] = useState(false);
-    const [amount, setAmount] = useState("");
-    const [purpose, setPurpose] = useState("");
-    const [duration, setDuration] = useState("");
-    return (
-          <div className="dashboard-layout">
-          <HamburgerButton
-               sidebarOpen={sidebarOpen}
-               onClick={() =>
-                    setSidebarOpen(!sidebarOpen)
-               }
-          />
-          <Sidebar
-               sidebarOpen={sidebarOpen}
-               setSidebarOpen={setSidebarOpen}
-          />
-          <main className="dashboard">
-               <DashboardHeader />
-               <section className="loan-page">
-                    <div className="loan-card">
-                    </div>
-               </section>
-          </main>
-          </div>
-)
+import TransactionList from "../components/dashboard/TransactionList";
+
+import AlertMessage from "../components/common/AlertMessage";
+
+import { applyLoan, getTransactions } from "../api/transactionApi";
+
+import "../styles/loan.css";
+
+function Loan() {
+const [loanAmount, setLoanAmount] = useState("");
+
+const [purpose, setPurpose] = useState("");
+
+const [transactions, setTransactions] = useState([]);
+
+const [alert, setAlert] = useState({
+  type: "",
+  message: "",
+});
+  return (
+    <DashboardLayout>
+
+      <DashboardHeader />
+
+    </DashboardLayout>
+  );
 }
+
+export default Loan;
