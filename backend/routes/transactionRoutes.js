12cb6989fc83a98bc0860b66aa9a8e80438b3477
Tamana543 <tamanafarzami33@gmail.com>
2026-07-12 14:37:26 +0600

    loan css updated, and loan API bug fixed

--- backend/routes/transactionRoutes.js
+++ backend/routes/transactionRoutes.js
@@ -1,9 +1,9 @@
 import express from "express";
 import protect from "../middleware/authMiddleware.js";
-import { depositMoney,getTransactions,transferMoney,withdrawMoney } from "../controllers/transactionController.js";
-import { applyLoan } from "../../frontend/src/api/transactionApi.js";
-
+// import { depositMoney,getTransactions,transferMoney,withdrawMoney } from "../controllers/transactionController.js";
+import { depositMoney, getTransactions, transferMoney, withdrawMoney, applyLoan } from "../controllers/transactionController.js";
 const router = express.Router();
+
 
 router.post("/deposit", protect, depositMoney );
 router.post("/transfer",protect,transferMoney)
