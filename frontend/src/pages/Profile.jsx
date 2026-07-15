c414ca86270706474c487c2addd6e431a936b237
Tamana543 <tamanafarzami33@gmail.com>
2026-07-15 16:55:46 +0600

    Profile.jsx and css added

---
+++ frontend/src/pages/Profile.jsx
@@ -0,0 +1,9 @@
+import DashboardLayout from "../components/dashboard/DashboardLayout";
+import DashboardHeader from "../components/dashboard/DashboardHeader";
+import { useAuth } from "../context/AuthContext";
+
+import "../styles/profile.css";
+
+function Profile() {
+     
+}
