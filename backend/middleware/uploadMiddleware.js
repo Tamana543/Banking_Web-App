1b6e8bcce4e496ec6181aef9af9981a3d479651a
Tamana543 <tamanafarzami33@gmail.com>
2026-07-16 20:20:54 +0600

    upladmiddleware updated

--- backend/middleware/uploadMiddleware.js
+++ backend/middleware/uploadMiddleware.js
@@ -1,26 +1,28 @@
-import multer from "multer"
-import path from "path"
+import multer from "multer";
+import path from "path";
 const storage = multer.diskStorage({
-    destination(req, file, cb) {
-        cb(null, "uploads/");
-    },
-    filename(req, file, cb) {
-        cb(
-            null,
-            `${Date.now()}${path.extname(file.originalname)}`
-        );
-    },
+  destination(req, file, cb) {
+    cb(null, "uploads/avatars");
+  },
+  filename(req, file, cb) {
+    const unique =
+      Date.now() +
+      "-" +
+      Math.round(Math.random() * 1e9);
+    cb(
+      null,
+      unique + path.extname(file.originalname)
+    );
+  },
 });
 const fileFilter = (req, file, cb) => {
-    if (file.mimetype.startsWith("image/")) {
-        cb(null, true);
-    }
-    else {
-        cb(new Error("Only image files are allowed."));
-    }
+  if (file.mimetype.startsWith("image/")) {
+    cb(null, true);
+  } else {
+    cb(new Error("Images only."), false);
+  }
 };
-const upload = multer({
-    storage,
-    fileFilter,
+export default multer({
+  storage,
+  fileFilter,
 });
-export default upload;
