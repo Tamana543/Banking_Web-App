0ae5cb06381f8613d2f6674ae307277fd937b641
Tamana543 <tamanafarzami33@gmail.com>
2026-07-17 17:24:53 +0600

    useeffect for action models added: Esc key for closing added

--- frontend/src/components/common/ActionModel.jsx
+++ frontend/src/components/common/ActionModel.jsx
@@ -1,4 +1,5 @@
 import "../../styles/components/modal.css"
+import { useEffect } from "react";
 function ActionModal({
   isOpen,
   title,
@@ -9,9 +10,27 @@ function ActionModal({
   loading = false
 }) {
   if (!isOpen) return null;
+
+  useEffect(() => {
+    const handleEsc = (e) => {
+        if (e.key === "Escape") {
+            onClose();
+        }
+    };
+    document.addEventListener(
+        "keydown",
+        handleEsc
+    );
+    return () =>
+        document.removeEventListener(
+            "keydown",
+            handleEsc
+        );
+}, [onClose]);
+
   return (
-    <div className="modal-overlay">
-      <div className="action-modal">
+    <div className="modal-overlay" onClick={onClose}>
+      <div className="action-modal" onClick={(e) => e.stopPropagation()}>
         <h2>{title}</h2>
         <div className="modal-body">
           {children}
