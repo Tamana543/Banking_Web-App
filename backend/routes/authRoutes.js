ddb61c2daef645150fafef98e9e184db54e1b508
Tamana543 <tamanafarzami33@gmail.com>
2026-07-18 17:46:46 +0600

    authRoutes.js updated: bakcend route added

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -2,13 +2,15 @@ import express from "express";
 import validateRegister from "../middleware/validateRegister.js";
 import protect from "../middleware/authMiddleware.js";
 import upload from "../middleware/uploadMiddleware.js";
+import { registerUser, loginUser, getCurrentUser, uploadAvatar,updateProfile,changePassword } from "../controllers/authController.js";
 
-import { registerUser, loginUser, getCurrentUser,uploadAvatar,updateProfile } from "../controllers/authController.js";
 const router = express.Router();
 router.post("/register", validateRegister, registerUser)
 router.post("/login", loginUser);
 router.get("/me",protect,getCurrentUser)
 router.post( "/avatar", protect, upload.single("avatar"), uploadAvatar );
+router.post("/change-password",protect,changePassword)
 router.put("/profile", protect, updateProfile);
+
 
 export default router
