d2a471e0e15f09d156fb9351c3b74845985ca25d
Tamana543 <tamanafarzami33@gmail.com>
2026-07-21 19:34:41 +0600

    toast.jsx added

---
+++ frontend/src/components/common/Toast.jsx
@@ -0,0 +1,17 @@
+import "../../styles/components/toast.css";
+
+function Toast({
+    type = "success",
+    message,
+    isVisible,
+}) {
+    if (!isVisible || !message) return null;
+
+    return (
+        <div className={`toast ${type}`}>
+            <span>{message}</span>
+        </div>
+    );
+}
+
+export default Toast;
