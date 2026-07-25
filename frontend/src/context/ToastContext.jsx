002496e2810d233a38c41d7acbc3b346aab0fd13
Tamana543 <tamanafarzami33@gmail.com>
2026-07-25 18:08:28 +0600

    ShowToast function updated: error handling switched to a more cleaner verion

--- frontend/src/context/ToastContext.jsx
+++ frontend/src/context/ToastContext.jsx
@@ -7,22 +7,59 @@ export function ToastProvider({ children }) {
         type: "success",
         visible: false,
     });
-    const showToast = (
-        message,
-        type = "success",
-        duration = 2500
-    ) => {
-        setToast({
-            message,
-            type,
-            visible: true,
-        });
-        setTimeout(() => {
-            setToast((prev) => ({
-                ...prev,
-                visible: false,
-            }));
-        }, duration);
+   const showToast = {
+        success(message, duration = 2500) {
+            setToast({
+                message,
+                type: "success",
+                visible: true,
+            });
+            setTimeout(() => {
+                setToast(prev => ({
+                    ...prev,
+                    visible: false,
+                }));
+            }, duration);
+        },
+        error(message, duration = 2500) {
+            setToast({
+                message,
+                type: "error",
+                visible: true,
+            });
+            setTimeout(() => {
+                setToast(prev => ({
+                    ...prev,
+                    visible: false,
+                }));
+            }, duration);
+        },
+        warning(message, duration = 2500) {
+            setToast({
+                message,
+                type: "warning",
+                visible: true,
+            });
+            setTimeout(() => {
+                setToast(prev => ({
+                    ...prev,
+                    visible: false,
+                }));
+            }, duration);
+        },
+        info(message, duration = 2500) {
+            setToast({
+                message,
+                type: "info",
+                visible: true,
+            });
+            setTimeout(() => {
+                setToast(prev => ({
+                    ...prev,
+                    visible: false,
+                }));
+            }, duration);
+        },
     };
     return (
         <ToastContext.Provider
