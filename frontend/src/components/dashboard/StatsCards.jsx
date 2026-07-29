bfd08d6b804febcb2d7fbe464f6b2ef1872aa4e5
Tamana543 <tamanafarzami33@gmail.com>
2026-07-29 22:26:40 +0600

    StatsCards updated: stats skeleton added

--- frontend/src/components/dashboard/StatsCards.jsx
+++ frontend/src/components/dashboard/StatsCards.jsx
@@ -1,7 +1,12 @@
 import "../../styles/dashboard/stats-card.css"
+import StatsCardsSkeleton from "../common/StatsSkeleton";
 function StatsCards({
-  transactions
+  transactions,
+  loading=false
 }) {
+    if (loading) {
+        return <StatsCardsSkeleton />;
+    }
   // total datas logic 
   const totalIncome = transactions
     .filter(transaction => transaction.type === "deposit")
@@ -10,7 +15,6 @@ function StatsCards({
             sum + Number(transaction.amount),
         0
     );
-
 const totalExpenses = transactions
     .filter(transaction => transaction.type === "withdrawal")
     .reduce(
@@ -25,59 +29,38 @@ const totalExpenses = transactions
             sum + Number(transaction.amount),
         0
     );
-
 const totalTransactions = transactions.length;
   return (
     <section className="stats-cards">
-
     <div className="stat-card income">
-
        
-
         <h4>Total Deposits</h4>
-
         <h2>
             $
             {totalIncome.toLocaleString()}
         </h2>
-
     </div>
-
     <div className="stat-card expense">
-
         <h4>Total Withdrawals</h4>
-
         <h2>
             $
             {totalExpenses.toLocaleString()}
         </h2>
-
     </div>
-
     <div className="stat-card transfer">
-
-
         <h4>Total Transfers</h4>
-
         <h2>
             $
             {totalTransfers.toLocaleString()}
         </h2>
-
     </div>
-
     <div className="stat-card transaction">
-
         <h4>Transactions</h4>
-
         <h2>
             {totalTransactions}
         </h2>
-
     </div>
-
 </section>
   );
 }
-
 export default StatsCards;
