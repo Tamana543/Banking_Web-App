d33f3c225ccffaa36af83934d584ad8c77bcfebb
Tamana543 <tamanafarzami33@gmail.com>
2026-07-30 19:51:06 +0600

    Setting.jsx updated: Setting.jsx first UI added

--- frontend/src/pages/Settings.jsx
+++ frontend/src/pages/Settings.jsx
@@ -6,11 +6,55 @@ function Settings() {
     <DashboardLayout>
       <DashboardHeader />
       <section className="settings-page">
-        <h2>Settings</h2>
-        <p className="settings-subtitle">
-          Manage your application preferences and account settings.
-        </p>
-      </section>
+    <h2>Settings</h2>
+    <p className="settings-subtitle">
+        Manage your application preferences.
+    </p>
+    <div className="settings-card">
+        <div className="setting-item">
+            <div>
+                <h4> Account</h4>
+                <p>Edit your profile information</p>
+            </div>
+            <span>›</span>
+        </div>
+        <div className="setting-item">
+            <div>
+                <h4> Security</h4>
+                <p>Password & PIN settings</p>
+            </div>
+            <span>›</span>
+        </div>
+        <div className="setting-item disabled">
+            <div>
+                <h4> Notifications</h4>
+                <p>Coming Soon</p>
+            </div>
+            <span>›</span>
+        </div>
+        <div className="setting-item disabled">
+            <div>
+                <h4>Banking Preferences</h4>
+                <p>Coming Soon</p>
+            </div>
+            <span>›</span>
+        </div>
+        <div className="setting-item disabled">
+            <div>
+                <h4> About</h4>
+                <p>Application information</p>
+            </div>
+            <span>›</span>
+        </div>
+        <div className="setting-item logout">
+            <div>
+                <h4> Logout</h4>
+                <p>Sign out of your account</p>
+            </div>
+            <span>›</span>
+        </div>
+    </div>
+</section>
     </DashboardLayout>
   );
 }
