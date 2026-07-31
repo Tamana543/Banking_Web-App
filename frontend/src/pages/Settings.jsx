025dbe5c2905fd234fee8eefc14f6ad3728a492a
Tamana543 <tamanafarzami33@gmail.com>
2026-07-31 20:13:00 +0600

    setting.jsx updated:  Account, profile and other performances links added

--- frontend/src/pages/Settings.jsx
+++ frontend/src/pages/Settings.jsx
@@ -1,7 +1,12 @@
+import { useNavigate } from "react-router-dom";
+import { useAuth } from "../context/AuthContext";
+
 import DashboardLayout from "../components/dashboard/DashboardLayout";
 import DashboardHeader from "../components/dashboard/DashboardHeader";
 import "../styles/settings.css";
 function Settings() {
+    const navigate = useNavigate();
+    const { logout } = useAuth();
   return (
     <DashboardLayout>
       <DashboardHeader />
@@ -11,14 +16,14 @@ function Settings() {
         Manage your application preferences.
     </p>
     <div className="settings-card">
-        <div className="setting-item">
+        <div className="setting-item" onClick={() => navigate("/profile")}>
             <div>
                 <h4> Account</h4>
                 <p>Edit your profile information</p>
             </div>
             <span>›</span>
         </div>
-        <div className="setting-item">
+        <div className="setting-item" onClick={() => navigate("/profile")}>
             <div>
                 <h4> Security</h4>
                 <p>Password & PIN settings</p>
@@ -39,14 +44,14 @@ function Settings() {
             </div>
             <span>›</span>
         </div>
-        <div className="setting-item disabled">
+        <div className="setting-item" onClick={() => navigate("/dashboard")}>
             <div>
                 <h4> About</h4>
                 <p>Application information</p>
             </div>
             <span>›</span>
         </div>
-        <div className="setting-item logout">
+        <div className="setting-item logout"  onClick={() => { logout(); navigate("/login") }}>
             <div>
                 <h4> Logout</h4>
                 <p>Sign out of your account</p>
