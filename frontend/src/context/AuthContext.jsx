65321d8067aa1583344fe7c38aba600b6433bdd4
Tamana543 <tamanafarzami33@gmail.com>
2026-08-25 14:46:40 +0600

    SavingGoal.js Added: saving feature officially started

--- frontend/src/context/AuthContext.jsx
+++ frontend/src/context/AuthContext.jsx
@@ -1,16 +1,12 @@
 import { createContext, useContext, useEffect, useState, useRef } from "react";
-
 const AuthContext = createContext();
-
 export function AuthProvider({ children }) {
   const [user, setUser] = useState(null);
   const [token, setToken] = useState(null);
   const logoutTimer = useRef(null); // Timer for the automatic logout 
-
   useEffect(() => {
     const storedUser = localStorage.getItem("user");
     const storedToken = localStorage.getItem("token");
-
     if (storedUser && storedToken) {
       setUser(JSON.parse(storedUser));
       setToken(storedToken);
@@ -29,23 +25,19 @@ export function AuthProvider({ children }) {
   const login = (userData, jwtToken) => {
     localStorage.setItem("user", JSON.stringify(userData));
     localStorage.setItem("token", jwtToken);
-
     setUser(userData);
     setToken(jwtToken);
     startLogoutTimer();
   };
-
   const logout = () => {
      if (logoutTimer.current) {
     clearTimeout(logoutTimer.current);
   }
     localStorage.removeItem("user");
     localStorage.removeItem("token");
-
     setUser(null);
     setToken(null);
   };
-
   return (
     <AuthContext.Provider
       value={{
@@ -55,11 +47,9 @@ export function AuthProvider({ children }) {
         logout,
         setUser,
   }}
-
     >
       {children}
     </AuthContext.Provider>
   );
 }
-
 export const useAuth = () => useContext(AuthContext);
