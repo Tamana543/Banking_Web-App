b8b19bdae62abe9a947bbdde4a09322a16feb1d2
Tamana543 <tamanafarzami33@gmail.com>
2026-08-27 18:28:11 +0600

    SavingGoal contrler udpated :addSavingsContribution added

--- backend/controllers/savingsGoalControllers.js
+++ backend/controllers/savingsGoalControllers.js
@@ -42,17 +42,89 @@ export const getSavingsGoals = async (req, res) => {
   try {
     const savingsGoals = await SavingsGoal.find({
       user: req.user._id,
-    }).sort({ createdAt: -1 });
+    }).sort({
+      createdAt: -1,
+    });
+
     res.status(200).json({
       success: true,
-      count: savingsGoals.length,
       savingsGoals,
     });
   } catch (error) {
     console.error("Get Savings Goals Error:", error);
+
     res.status(500).json({
       success: false,
       message: "Server error.",
     });
   }
 };
+export const addSavingsContribution = async (req, res) => {
+  try {
+    const { amount } = req.body;
+     if (
+      amount === undefined ||
+      Number(amount) <= 0
+    ) {
+      return res.status(400).json({
+        success: false,
+        message: "Contribution amount must be greater than 0.",
+      });
+    }
+
+    const contributionAmount = Number(amount);
+    const savingsGoal = await SavingsGoal.findOne({
+      _id: req.params.id,
+      user: req.user._id,
+    });
+
+    if (!savingsGoal) {
+      return res.status(404).json({
+        success: false,
+        message: "Savings goal not found.",
+      });
+    }
+    if (savingsGoal.status === "completed") {
+      return res.status(400).json({
+        success: false,
+        message: "This savings goal has already been completed.",
+      });
+    }
+    const remainingAmount = savingsGoal.targetAmount -  savingsGoal.currentAmount;
+     if (contributionAmount > remainingAmount) {
+      return res.status(400).json({
+        success: false,
+        message: `You only need $${remainingAmount.toFixed(
+          2
+        )} to complete this goal.`,
+      });
+    }
+    savingsGoal.currentAmount += contributionAmount;
+     if (
+      savingsGoal.currentAmount >=
+      savingsGoal.targetAmount
+    ) {
+      savingsGoal.currentAmount =
+        savingsGoal.targetAmount;
+
+      savingsGoal.status = "completed";
+    }
+    await savingsGoal.save();
+     res.status(200).json({
+      success: true,
+      message: "Savings contribution added successfully.",
+      savingsGoal,
+    });
+  } catch (error) {
+    console.error(
+      "Add Savings Contribution Error:",
+      error
+    );
+
+    res.status(500).json({
+      success: false,
+      message: "Server error.",
+    });
+  }
+};
+
