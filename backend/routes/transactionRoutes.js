2782ee22f8c941c98f3ed82515f6c6609172313b
Tamana543 <tamanafarzami33@gmail.com>
2026-08-31 11:53:12 +0600

    transactionRoutes updated: saving routes backend added next front API

--- backend/routes/transactionRoutes.js
+++ backend/routes/transactionRoutes.js
@@ -1,14 +1,12 @@
 import express from "express";
 import protect from "../middleware/authMiddleware.js";
-// import { depositMoney,getTransactions,transferMoney,withdrawMoney } from "../controllers/transactionController.js";
-import { depositMoney, getTransactions, transferMoney, withdrawMoney, applyLoan } from "../controllers/transactionController.js";
+import { depositMoney, getTransactions, transferMoney, withdrawMoney, applyLoan, addMoneyToSavingsGoal, } from "../controllers/transactionController.js";
 const router = express.Router();
-console.log("Transaction routes loaded");
 
-router.post("/deposit", protect, depositMoney );
-router.post("/transfer",protect,transferMoney)
-router.post("/withdraw",protect,withdrawMoney)
-router.post("/loan",protect,applyLoan)
-router.get("/",protect,getTransactions)
-
+router.post("/deposit", protect, depositMoney);
+router.post("/transfer", protect, transferMoney);
+router.post("/withdraw", protect, withdrawMoney);
+router.post("/loan", protect, applyLoan);
+router.post( "/savings-contribution", protect, addMoneyToSavingsGoal );
+router.get("/", protect, getTransactions);
 export default router;
