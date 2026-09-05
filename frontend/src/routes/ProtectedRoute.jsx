d011f6c12b8b02fa5d040777fe341f88165ee5df
Tamana543 <tamanafarzami33@gmail.com>
2026-09-05 11:38:38 +0600

    ProtectedRoutes.jsx updated

--- frontend/src/routes/ProtectedRoute.jsx
+++ frontend/src/routes/ProtectedRoute.jsx
@@ -1,6 +1,13 @@
 import { Navigate } from "react-router-dom";
+import { useAuth } from "../context/AuthContext";
 function ProtectedRoute({ children }) {
-  const isLoggedIn = localStorage.getItem("user");
-  return isLoggedIn ? children : <Navigate to="/login" />;
+  const { user, token, authLoading } = useAuth();
+  if (authLoading) {
+    return null;
+  }
+  if (!user || !token) {
+    return <Navigate to="/login" replace />; // replace: no invalid prtected routes stored in local storage
+  }
+  return children;
 }
 export default ProtectedRoute;
