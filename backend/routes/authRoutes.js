4ca401baee9a3fc2929b48be5277434259928bde
Tamana543 <tamanafarzami33@gmail.com>
2026-09-07 17:09:09 +0600

    authRoutes.js updated: pin verification added

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,17 +1,13 @@
 import express from "express";
-import validateRegister from "../middleware/validateRegister.js";
 import protect from "../middleware/authMiddleware.js";
-import upload from "../middleware/uploadMiddleware.js";
-import { registerUser, loginUser, getCurrentUser, uploadAvatar,updateProfile,changePassword,changePin } from "../controllers/authController.js";
-
+import verifyPin from "../middleware/pinMiddleware.js";
+import { registerUser, loginUser, getCurrentUser, uploadAvatar, updateProfile, changePassword, changePin, } from "../controllers/authController.js";
 const router = express.Router();
-router.post("/register", validateRegister, registerUser)
+router.post("/register", registerUser);
 router.post("/login", loginUser);
-router.get("/me",protect,getCurrentUser)
-router.post( "/avatar", protect, upload.single("avatar"), uploadAvatar );
-router.put("/profile", protect, updateProfile);
-router.put("/change-password",protect,changePassword)
-router.put("/change-pin",protect,changePin)
-
-
-export default router
+router.get( "/me", protect, getCurrentUser );
+router.post( "/avatar", protect, uploadAvatar );
+router.put( "/profile", protect, updateProfile);
+router.put( "/change-password", protect, changePassword );
+router.put( "/change-pin", protect, verifyPin("currentPin"), changePin );
+export default router;
