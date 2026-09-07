d1053bc23caa26258579b940985a75ff95bb40bb
Tamana543 <tamanafarzami33@gmail.com>
2026-09-07 17:06:10 +0600

    transactionRoutes.js updated: pin secure middleware checker added

--- backend/routes/transactionRoutes.js
+++ backend/routes/transactionRoutes.js
@@ -1,12 +1,12 @@
 import express from "express";
 import protect from "../middleware/authMiddleware.js";
+import verifyPin from "../middleware/pinMiddleware.js";
 import { depositMoney, getTransactions, transferMoney, withdrawMoney, applyLoan, addMoneyToSavingsGoal, } from "../controllers/transactionController.js";
 const router = express.Router();
-
-router.post("/deposit", protect, depositMoney);
-router.post("/transfer", protect, transferMoney);
-router.post("/withdraw", protect, withdrawMoney);
-router.post("/loan", protect, applyLoan);
-router.post( "/savings-contribution", protect, addMoneyToSavingsGoal );
-router.get("/", protect, getTransactions);
+router.post( "/deposit", protect, depositMoney );
+router.post( "/transfer", protect, verifyPin(), transferMoney );
+router.post( "/withdraw", protect, verifyPin(), withdrawMoney );
+router.post( "/loan", protect, verifyPin(), applyLoan );
+router.post( "/savings-contribution", protect, verifyPin(), addMoneyToSavingsGoal );
+router.get( "/", protect, getTransactions );
 export default router;
