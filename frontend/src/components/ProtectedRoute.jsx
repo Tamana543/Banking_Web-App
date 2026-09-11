e556d5f50b1432b3cb45d89bf6f98ed42d8f3ad6
Tamana543 <tamanafarzami33@gmail.com>
2026-09-11 18:05:23 +0600

    files updated: just checking the steps how much has been done before comming to this step

--- frontend/src/components/ProtectedRoute.jsx
+++ frontend/src/components/ProtectedRoute.jsx
@@ -1,14 +1,13 @@
 import { Navigate } from "react-router-dom";
 import { useAuth } from "../context/AuthContext";
-
 function ProtectedRoute({ children }) {
-  const { user } = useAuth();
-
-  if (!user) {
+  const { user, token, authLoading } = useAuth();
+  if (authLoading) {
+    return null;
+  }
+  if (!user || !token) {
     return <Navigate to="/login" replace />;
   }
-
   return children;
 }
-
 export default ProtectedRoute;
