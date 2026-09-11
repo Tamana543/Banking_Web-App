34c929825198c5be50e2fa9df435b98660f6db7b
Tamana543 <tamanafarzami33@gmail.com>
2026-09-11 18:24:11 +0600

    step one of API validation :Input validation started

--- backend/server.js
+++ backend/server.js
@@ -4,42 +4,31 @@ import connectDB from "./config/db.js";
 import dns from "node:dns";
 import cors from "cors"
 import path from "path";
-
 // routes
 import authRouter from "./routes/authRoutes.js";
 import transactionRoutes from "./routes/transactionRoutes.js"
 import savingsGoalRoutes from "./routes/savingsGoalRoutes.js"
-
 dns.setServers(["1.1.1.1", "8.8.8.8"]);
-
 dotenv.config();
-
 connectDB();
-
 const app = express();
-
 app.use(
   cors({
     origin: "http://localhost:5173",
     credentials: true,
   })
 );
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
 // APIs 
 app.use("/api/auth", authRouter);
 app.use("/api/transactions", transactionRoutes);
 app.use("/api/savings-goals", savingsGoalRoutes);
 app.use( "/uploads", express.static(path.join(process.cwd(), "uploads")));
-
 app.get("/", (req, res) => {
   res.send("Bankist API Running...");
 });
-
 const PORT = process.env.PORT || 5000;
-
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
